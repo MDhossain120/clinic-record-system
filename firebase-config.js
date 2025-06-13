7261da9a9751833beb8f5c9cba377b3a426b64b7
MDhossain120 <abeerhossin@gmail.com>
2025-06-14 02:26:19 +0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
-// আপনার Firebase config দিয়ে এখানে বসিয়ে দিন
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  appId: "YOUR_APP_ID",
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqjaLgabOuFb6k8LIJXovKOZMjtxc3CjU",
+  authDomain: "clinicapp-a2f2a.firebaseapp.com",
+  projectId: "clinicapp-a2f2a",
+  storageBucket: "clinicapp-a2f2a.firebasestorage.app",
+  messagingSenderId: "313561643437",
+  appId: "1:313561643437:web:194a95f5667928cfe78924",
+  measurementId: "G-YB9QF191NY"
 };
-
-firebase.initializeApp(firebaseConfig);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+export const app = initializeApp(firebaseConfig);
